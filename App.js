ad8c22e85aa58f10512dfc570ee84f590ac92ad5
Miguel Luna <6733909-santiago.luna@users.noreply.gitlab.com>
2022-03-03 15:37:29 -0600

    first commit

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={styles.container}>
+      <Text style={}>Hello world!</Text>
+      <Text>Hello world!</Text>
+      <Text>Hello world!</Text>
+      <Text>Hello world!</Text>
+      <Text>Hello world!</Text>
+      <Text>Hello world!</Text>
+      <Text>Hello world!</Text>
+      <Text>Hello world!</Text>
+      <Text>Hello world!</Text>
+    </View>
+  );
+};
+
+const styles = {
+  container:{
+    flex:1,
+    justifyContent:'flex-start',
+    alignItems: 'flex-start',
+  },
+  text:{
+
+  }
+}
+
+export default App;
---
+++ windows/ProjectTest/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::ProjectTest::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::ProjectTest::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
